f65d5a50300e9a1ae4ba4373d5d18fe9d8b5adb9
JetShen <fernandoarielthielelepe@gmail.com>
2023-04-24 18:05:11 -0400

    manga tab

--- webScraping/ext_page.py
+++ webScraping/ext_page.py
@@ -7,6 +7,8 @@ def Manga(link):
     title = soup.select_one('#main_content > div:nth-child(2) > div.row.no-gutters > div.col-12.p-2 > span.releasestitle.tabletitle').text
     img =soup.select_one('#main_content > div:nth-child(2) > div.row.no-gutters > div:nth-child(4) > div:nth-child(2) > center > img')['src']
 
+    img_split = img.rsplit('/',1) 
+
     lastChapters  = soup.select_one('#main_content > div:nth-child(2) > div.row.no-gutters > div:nth-child(3) > div:nth-child(17) > i:nth-child(1)')
     try:
         lastChapters  = lastChapters .text
@@ -15,17 +17,26 @@ def Manga(link):
             lastChapters  = soup.select_one('#main_content > div:nth-child(2) > div.row.no-gutters > div:nth-child(3) > div:nth-child(20)').text
             lastChapters .replace('\n', '')
 
-    genres = []
+    Genders = []
+    pop = False
     for i in range(1, 5):
-        g = soup.select_one('#main_content > div:nth-child(2) > div.row.no-gutters > div:nth-child(4) > div:nth-child(5) > a:nth-child('+str(i)+') > u').text
-        genres.append(str(g))
+        try:
+            g = soup.select_one('#main_content > div:nth-child(2) > div.row.no-gutters > div:nth-child(4) > div:nth-child(5) > a:nth-child('+str(i)+') > u').text
+            Genders.append(str(g))
+        except:
+            pop=True
+
+    if(pop):
+        Genders.pop()
+
 
     manga = {
         'title': str(title),
         'url': str(link),
-        'img': str(img),
-        'Chapters ': str(lastChapters ),
-        'genres': genres
+        'img_link': str(img),
+        'img_path':str(img_split[1]),
+        'Chapters': str(lastChapters ),
+        'Genders': Genders
     }
 
     return manga
--- widgets/manga.py
+++ widgets/manga.py
@@ -1,35 +1,64 @@
 from PySide6.QtWidgets import *
 from PySide6.QtCore import Qt
+from PySide6.QtGui import QPixmap
 from jsonUtils.json_utils import ReadS
+from PySide6.QtCore import QTimer
+import requests
 
 class About_manga(QWidget):
+    
     def __init__(self):
         super().__init__()
-        #about the manga selected
+        
+
         layout = QVBoxLayout()
         layout.setAlignment(Qt.AlignCenter)
-        try:
-            self.manga = ReadS()
-            
-            
-            title = "Title: " + self.manga['title']
-            self.manga_title = QLabel()
-            layout.addWidget(self.manga_title)
 
-            chapters = "Chapters: " + self.manga['chapters']
-            self.Chapthers = QLabel(str(chapters))
-            layout.addWidget(self.Chapthers)
+        self.title = QLabel("Manga Title:")
+        layout.addWidget(self.title)
 
-            self.prueba  = QPushButton("prueba")
-            layout.addWidget(self.prueba)
-            self.prueba.clicked.connect(self.test)
+        self.img = QLabel(self)
+        layout.addWidget(self.img)
 
-        except:
-            self.selected = QLabel("No manga selected")
-            layout.addWidget(self.selected)
-            
+        self.genders = QLabel("Tag, Tag")
+        layout.addWidget(self.genders)
+
+        self.chapters = QLabel("Chapters: ")
+        layout.addWidget(self.chapters)
+        
+        self.btn_refresh = QPushButton("Refresh")
+        layout.addWidget(self.btn_refresh)
+        self.btn_refresh.clicked.connect(self.refresh)
+
         self.setLayout(layout)
 
 
-    def test(self):
-        print("object: ", self.manga)
+    
+    def refresh(self):
+        try:
+            manga = ReadS()
+            
+            #download the img of the manga
+            response = requests.get(manga['img_link'], stream=True)
+            response.raise_for_status()
+            with open('img/'+str(manga['img_path']), "wb") as archivo:
+                for chunk in response.iter_content(chunk_size=8192):
+                    archivo.write(chunk)
+
+            self.title.setText(f"Manga title: {manga['title']}")
+
+            pixel = QPixmap("img/"+str(manga['img_path']))
+            self.img.setPixmap(pixel)
+            self.chapters.setText(f"Chapters: {manga['Chapters']}")
+            self.genders.setText(f"Genders: {manga['Genders']}")
+                        
+        except:
+            print("failed to load manga")
+
+        
+
+
+        
+
+
+
