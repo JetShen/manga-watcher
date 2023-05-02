92e2b6fcb55b14f7b29c509add832280c45da50d
JetShen <fernandoarielthielelepe@gmail.com>
2023-05-01 23:39:25 -0400

    Library update

---
+++ webScraping/actualizar.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+
+def Manga(link):
+    page = requests.get(str(link))
+    soup = BeautifulSoup(page.text ,'html.parser')
+    title = soup.select_one('#main_content > div:nth-child(2) > div.row.no-gutters > div.col-12.p-2 > span.releasestitle.tabletitle').text
+    img =soup.select_one('#main_content > div:nth-child(2) > div.row.no-gutters > div:nth-child(4) > div:nth-child(2) > center > img')['src']
+
+    img_split = img.rsplit('/',1) 
+
+    lastChapters  = soup.select_one('#main_content > div:nth-child(2) > div.row.no-gutters > div:nth-child(3) > div:nth-child(17) > i:nth-child(1)')
+    try:
+        lastChapters  = lastChapters .text
+    except:
+        if ( lastChapters  == None):
+            lastChapters  = soup.select_one('#main_content > div:nth-child(2) > div.row.no-gutters > div:nth-child(3) > div:nth-child(20)').text
+            lastChapters .replace('\n', '')
+
+    Genders = []
+    pop = False
+    for i in range(1, 5):
+        try:
+            g = soup.select_one('#main_content > div:nth-child(2) > div.row.no-gutters > div:nth-child(4) > div:nth-child(5) > a:nth-child('+str(i)+') > u').text
+            Genders.append(str(g))
+        except:
+            pop=True
+
+    if(pop):
+        Genders.pop()
+
+
+    manga = {
+        'title': str(title),
+        'url': str(link),
+        'img_link': str(img),
+        'img_path':str(img_split[1]),
+        'Chapters': str(lastChapters ),
+        'Genders': Genders
+    }
+
+    return manga
+
--- widgets/library.py
+++ widgets/library.py
@@ -1,35 +1,76 @@
 from PySide6.QtWidgets import *
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QWidget, QLabel
 from PySide6.QtGui import QPixmap
 from jsonUtils.json_utils import ReadF, WriteS
-
 
 class LibraryWidget(QWidget):
     def __init__(self):
         super().__init__()
 
-        layout = QGridLayout()
-        library = ReadF()
-        for manga in range(len(library)):
-            pixmap = QPixmap(f"img/{library[manga]['img_path']}")
-            
-            label = QLabel()
-            label.setPixmap(pixmap)
-            label.setScaledContents(True)
+        #Add refresh button
+        botonRefrescar = QPushButton("Refrescar")
+        botonRefrescar.setStyleSheet("background-color: red; color: white;")
+        botonRefrescar.clicked.connect(self.cLayout)
+        layoutBoton = QHBoxLayout()
+        layoutBoton.addStretch()
+        layoutBoton.addWidget(botonRefrescar)
+        layoutBoton.addStretch()
 
-            label.setMouseTracking(True)
-            label.setCursor(Qt.PointingHandCursor)#change cursor to pointing hand cursor
+        
+        layoutFrontal = QVBoxLayout()
+        self.layoutCentral = QGridLayout()
 
-            #connect the function to the manga object using lambda
-            label.mousePressEvent = lambda event, idx=manga: self.selected(event, idx, library)
+        layoutFrontal.addLayout(layoutBoton)
+        layoutFrontal.addLayout(self.layoutCentral)
 
-            #add to the grid
-            layout.addWidget(label, manga // 2, manga % 2)
+        self.cLayout()
 
-        self.setLayout(layout)
+        self.setLayout(layoutFrontal)
 
     def selected(self, event, index, library):
         manga = library[index]
         WriteS(manga)
 
+    
+    def cLayout(self):
+
+        library = ReadF()
+        for manga in library:
+            pass
+            #add system to update
+
+
+        for manga in range(len(library)):
+            LyManga = QVBoxLayout()#Make the layout for the manga object
+
+            #Add the title to the layout
+            title = QLabel(str(library[manga]['title']))
+            title.setStyleSheet("background-color: blue; color: white;")
+            LyManga.addWidget(title)
+
+            #Add the image to the layout
+            pixmap = QPixmap(f"img/{library[manga]['img_path']}")
+            img = QLabel()
+            img.setStyleSheet("background-color: blue; color: white;")
+            img.setPixmap(pixmap)
+            img.setScaledContents(True)
+            img.setMouseTracking(True)
+            img.setCursor(Qt.PointingHandCursor)#change cursor to pointing hand cursor
+            #connect the function to the manga object using lambda
+            img.mousePressEvent = lambda event, idx=manga: self.selected(event, idx, library)
+            LyManga.addWidget(img)
+
+            #Add the chapters to the layout
+            chapters = QLabel(f"Chapters: {library[manga]['Chapters']}")
+            chapters.setStyleSheet("background-color: blue; color: white;")
+            LyManga.addWidget(chapters)
+
+            LyManga.setSpacing(0)
+
+            #add to the grid int(rows) and int(columns) 
+            self.layoutCentral.addLayout(LyManga, manga // 2, manga % 2)
+        
+        return self.layoutCentral
+
+if __name__ == '__main__':
+    pass
